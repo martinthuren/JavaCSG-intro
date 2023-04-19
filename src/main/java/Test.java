acc1629cccb4cc5bd3cb2e0d5e276bc6ab366373
martinthuren <cph-mc489@cphbusiness.dk>
2023-04-19 10:50:19 +0200

    cross

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,13 +1,19 @@
-import org.abstractica.javacsg.Geometry3D;
-import org.abstractica.javacsg.JavaCSG;
-import org.abstractica.javacsg.JavaCSGFactory;
+import org.abstractica.javacsg.*;
 
 public class Test
 {
 	public static void main(String[] args)
 	{
 		JavaCSG csg = JavaCSGFactory.createDefault();
-		Geometry3D cyl = csg.cylinder3D(10, 20, 32, true);
-		csg.view(cyl);
+
+		Geometry3D cross = csg.box3D(45,15,5,true);
+		Geometry3D cross2 = csg.box3D(15,45,5,true);
+
+		Geometry3D cross3 = csg.union3D(cross, cross2);
+
+		csg.view(cross3);
+
+
+
 	}
 }
